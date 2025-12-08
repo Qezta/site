a3032fa7d49b02de6e944a0f1d11844f29eb5287
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-12-08 15:53:27 +0530

    refactor(major): move from a multi-branch to a monorepo for subdomain path

---
+++ static/404.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+	<meta charset='utf-8'>
+	<meta property='og:image' content='https://divit.site/resources/smallLogo.png'/>
+	<meta property="og:site_name" content="Divit Mittal">
+	<meta property="og:url" content="https://divit.site">
+	<meta property="og:type" content="website">
+	<meta property='og:image:alt' content='Abstract Logo'/>
+	<meta property='og:title' content='n/a'/>
+	<meta property='og:description' content="n/a"/>
+	<title>404</title>
+	<link rel="icon" href="resources/icon.png">
+	<link rel="stylesheet" href="/styles/404.css">
+	<script type="text/javascript" src="/scripts/404.js"></script>
+</head>
+<body>
+<div class="container">
+	<span class="text">404</span>
+</div>
+</body>
+</html>
